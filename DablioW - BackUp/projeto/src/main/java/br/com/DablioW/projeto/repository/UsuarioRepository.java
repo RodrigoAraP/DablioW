1a218ff96bab24fc98a4bb8f31e300fd3832c393
Rodrigo Araújo Passos <133711269+RodrigoAraP@users.noreply.github.com>
2023-11-23 21:54:52 -0300

    Delete DablioW - BackUp directory

--- DablioW - BackUp/projeto/src/main/java/br/com/DablioW/projeto/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.DablioW.projeto.repository;
-
-import br.com.DablioW.projeto.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Usuario findByNomeAndSenha(String nome, String senha);
-}
